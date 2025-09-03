c493d8006de69b6075a00f0a132fef7fa6d5807c
Sanjay8-ht <sanjay@holithemes.com>
2025-09-03 11:30:24 +0530

    pr

--- testing.js
+++ testing.js
@@ -1,39 +1,39 @@
 // error.js
 
 // 1. Syntax Error (missing closing parenthesis)
-function brokenSyntax() {
-    console.log("This will cause a syntax error");
+function brokenSyntax () {
+	console.log( 'This will cause a syntax error' );
 }
 
+const nonExistentVar = '';
+
 // 2. Reference Error (undefined variable)
-function brokenReference() {
-    console.log(nonExistentVar);
+function brokenReference () {
+	console.log( nonExistentVar );
 }
 
 // 3. Type Error (calling a number as a function)
-function brokenType() {
-    let num = 42;
-    num(); // will throw TypeError
+function brokenType () {
+	const num = 42;
+	num(); // will throw TypeError
 }
 
 // Run them one by one
 try {
-    brokenSyntax();
-} catch (err) {
-    console.error("Caught Syntax Error:", err);
+	brokenSyntax();
+} catch ( err ) {
+	console.error( 'Caught Syntax Error:', err );
 }
 
 try {
-    brokenReference();
-} catch (err) {
-    console.error("Caught Reference Error:", err);
+	brokenReference();
+} catch ( err ) {
+	console.error( 'Caught Reference Error:', err );
 }
 
 try {
-    brokenType();
-} catch (err) {
-    console.error("Caught Type Error:", err);
+	brokenType();
+} catch ( err ) {
+	console.error( 'Caught Type Error:', err );
 }
-
-
 
