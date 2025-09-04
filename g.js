bd44d976c0f19db9f7b4fdd3ef2aa57c1a9a2c48
harshitha7-ht <harshitha@holithemes.com>
2025-09-04 10:44:49 +0530

    g file

---
+++ g.js
@@ -0,0 +1,24 @@
+// eslint security rules should flag these patterns
+
+const { exec } = require('child_process');
+const http = require('http');
+
+http.createServer((req, res) => {
+const url = new URL(req.url, '[http://localhost](http://localhost/)');
+const user = url.searchParams.get('q') || 'echo hello';
+
+// 1) Command injection
+exec(user, (err, stdout) => {
+if (err) return res.end('err');
+res.write(stdout);
+});
+
+// 2) eval with expression (remote code execution)
+//    e.g. /?q=console.log("pwned")
+eval(user);
+
+// 3) Non-literal RegExp (potential ReDoS if attacker controls pattern)
+//    e.g. /?q=(a+)+$
+const r = new RegExp(user);
+res.end('done');
+}).listen(3000);
